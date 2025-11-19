4d2c952d04fa43c05affbb7f18dd8c0c62f99ad5
TushrS07 <124758085+TushrS07@users.noreply.github.com>
2025-11-20 04:15:59 +0530

    logs

--- server/config/email.js
+++ server/config/email.js
@@ -2,6 +2,13 @@ import nodemailer from 'nodemailer';
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("------ SMTP DEBUG CHECK ------");
+console.log("SMTP_HOST:", JSON.stringify(process.env.SMTP_HOST));
+console.log("EMAIL:", JSON.stringify(process.env.EMAIL));
+console.log("PASSWORD exists:", !!process.env.PASSWORD);
+console.log("------------------------------");
+
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 587,
