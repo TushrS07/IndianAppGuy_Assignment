d55eefdaa7c550ed362ea87aac34c52214dc4c59
TushrS07 <124758085+TushrS07@users.noreply.github.com>
2025-11-20 04:10:00 +0530

    transport changes

--- server/config/email.js
+++ server/config/email.js
@@ -5,13 +5,13 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 587,
-  secure: false, // or true if you use port 465
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD
   },
-  logger: true,   // logs to console
-  debug: true      // include SMTP traffic
+  logger: true,
+  debug: true
 });
 
-export default transport;
+export default transporter;
